360b81f49505d8e4520c4ab63962abc5fb505437
LKWLaLa <lkwlala@gmail.com>
2018-09-02 00:23:37 -0400

    set up basic auth with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :payments
   has_many :sales
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
