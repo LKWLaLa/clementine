6595cf204c8b9487a62d646c18683ece760cf3cb
LKWLaLa <lkwlala@gmail.com>
2018-09-02 18:29:47 -0400

    customize user index

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,7 +2,15 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :email
+permit_params :first_name, :last_name, :email
+
+  index do |user|
+    column :first_name
+    column :last_name
+    column :email
+    column :city
+    column :country
+  end
 
 
 end
