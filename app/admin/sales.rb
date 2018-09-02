1bd6709a6e6cea3029a5c528eb06be460255c61e
LKWLaLa <lkwlala@gmail.com>
2018-09-02 18:59:25 -0400

    fix sales display in admin.  add link to log in as admin

--- app/admin/sales.rb
+++ app/admin/sales.rb
@@ -2,14 +2,13 @@ ActiveAdmin.register Sale do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+permit_params :user_id, :item_id, :price_id, :payment_id
+
+  index do |user|
+    column :user_id
+    column :item_id
+    column :price_id
+    column :payment_id
+  end
 
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,4 +3,9 @@ class Sale < ApplicationRecord
   belongs_to :user
   belongs_to :item
   belongs_to :payment
+
+  #display name for active admin
+  def display_name
+    id
+  end
 end
