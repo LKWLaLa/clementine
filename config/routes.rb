2c09cca65ccae148a2abaca00a9024798df0f155
LKWLaLa <lkwlala@gmail.com>
2018-09-10 21:06:14 -0400

    only need route for charges#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   namespace :api, defaults: { format: :json } do
-    resources :charges
+    resources :charges, only: [:create]
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
