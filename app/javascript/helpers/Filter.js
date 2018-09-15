de628827bf1c57717efc03e7c982fa20bcb9bed1
Chad Mills <chad.mills@gmail.com>
2018-09-14 20:37:13 -0400

    implement and test filtering functions

---
+++ app/javascript/helpers/Filter.js
@@ -0,0 +1,62 @@
+class Filter {
+	static excludedItems(priorItemIds,exclusions) {
+	  return new Set(exclusions
+	    .filter(e => priorItemIds.has(e.excluderItemId))
+	    .map(e => e.excludedItemId))
+	}
+
+	static upgradeToItems(priorItemIds,upgrades) {
+	  return new Set(upgrades
+	    .filter(u => priorItemIds.has(u.upgradeFromItemId))
+	    .map(u => u.upgradeToItemId))
+	}
+
+	// items that, based on priorItemIds, the user is eligible to purchase because
+	// (1) item i requires no qualifications or
+	// (2) one of priorItemIds qualifies the user to purchase i
+	static eligibleItems(priorItemIds,itemIds,qualifications) {
+	  // items that the user has qualified to purchases based on priorItems
+	  let qualifiedItemIds = new Set(qualifications
+	    .filter(q => priorItemIds.has(q.qualifierItemId))
+	    .map(q => q.qualifiedItemId))
+
+	  // items that require a qualifying purchase
+	  let restrictedItemIds = new Set(qualifications
+	    .map(q => q.qualifiedItemId))
+
+	  // items that do not require a qualifying purchase
+	  let unrestrictedItemIds = new Set([...itemIds]
+	    .filter(i => !restrictedItemIds.has(i)))
+
+	  return new Set([...qualifiedItemIds,...unrestrictedItemIds])
+	}
+
+	// Items that are not excluded or upgrade_to items (but may not be qualified for yet)
+	// These are the items that should be displayed on the purchaseableItems Table, given
+	// that the user has purchased prior_items.
+	static purchaseableItems(purchasedItemIds,itemIds,exclusions,upgrades) {
+	  return new Set([...itemIds]
+	    .filter(i => !this.excludedItems(purchasedItemIds,exclusions).has(i))
+	    .filter(i => !this.upgradeToItems(purchasedItemIds,upgrades).has(i)))
+	}
+
+	// Items that appear as enabled on the purchaseable items table
+	// return set should contain (in priority order):
+	// 	only purchaseablItems(purchasedItemIds,...)
+	// 	all selectedItemIds
+	// 	no items excluded by or upgradeable from the selectedItems (except the selectedItems themselves)
+	//  only items for which the user is eligible, based on purchased items and selected items
+	static enabledPurchaseableItems(purchasedItemIds,
+	    selectedItemIds,
+	    itemIds,
+	    exclusions,
+	    upgrades,
+	    qualifications) {
+	  let priorItemIds = new Set([...purchasedItemIds,...selectedItemIds])
+	  let a = new Set([...this.purchaseableItems(priorItemIds,itemIds,exclusions,upgrades)]
+	    .filter(i => this.eligibleItems(priorItemIds,itemIds,qualifications).has(i)))
+	  return new Set([...a,...selectedItemIds])
+	}
+}
+
+export default Filter;
---
+++ spec/javascript/filter.spec.js
@@ -0,0 +1,135 @@
+import Filter from 'helpers/Filter.js'
+
+let itemIds = new Set()
+let exclusions = []
+let upgrades = []
+let qualifications = []
+let purchasedItemIds = new Set()
+let priorItemIds = new Set()
+
+
+describe('excludedItems', () => {
+	it('should return the excludedItems for the priorItemIds', () => {
+		let exclusions = [
+			{excluderItemId: 1,excludedItemId: 3},
+			{excluderItemId: 2,excludedItemId: 5},
+			{excluderItemId: 2,excludedItemId: 6}
+		]
+		expect([...Filter.excludedItems(new Set([2,4]),exclusions)].sort()).toEqual([5,6])
+	})
+
+	it('should work with different exclusions', () => {
+		let exclusions = [
+			{excluderItemId: 1, excludedItemId: 1},
+			{excluderItemId: 2, excludedItemId: 2},
+			{excluderItemId: 1, excludedItemId: 2}
+		]
+		expect([...Filter.excludedItems(new Set([2]),exclusions)].sort()).toEqual([2])
+		expect([...Filter.excludedItems(new Set([1]),exclusions)].sort()).toEqual([1,2])
+	})
+})
+
+describe('upgradeToItems', () => {
+	beforeEach(() => {
+		priorItemIds = new Set([2,4])
+		upgrades = [
+			{upgradeFromItemId: 1,upgradeToItemId: 3},
+			{upgradeFromItemId: 2,upgradeToItemId: 5},
+			{upgradeFromItemId: 2,upgradeToItemId: 6}
+		]
+	})
+
+	it('should return the upgradeToItems for the priorItemIds', () => {
+		expect([...Filter.upgradeToItems(priorItemIds,upgrades)].sort()).toEqual([5,6])
+	})
+})
+
+describe('eligibleItems', () => {
+	beforeEach(() => {
+		itemIds = new Set([1,2,3,4,5])
+		qualifications = [
+			{qualifierItemId: 1, qualifiedItemId: 5},
+			{qualifierItemId: 2, qualifiedItemId: 4},
+			{qualifierItemId: 3, qualifiedItemId: 4}
+		]		
+	})
+	
+	it('should return the eligibleItems for the priorItemIds and the itemIds', () => {
+		expect([...Filter.eligibleItems(new Set(),itemIds,qualifications)].sort()).toEqual([1,2,3])
+		expect([...Filter.eligibleItems(new Set([2]),itemIds,qualifications)].sort()).toEqual([1,2,3,4])
+	})
+})
+
+describe('purchaseableItems', () => {
+	beforeEach(() => {
+		itemIds = new Set([1,2,3,4])
+		exclusions = [
+			{excluderItemId: 1, excludedItemId: 1},
+			{excluderItemId: 2, excludedItemId: 2},
+			{excluderItemId: 1, excludedItemId: 2}
+		]
+		upgrades = [
+			{upgradeFromItemId: 2, upgradeToItemId: 1},
+			{upgradeFromItemId: 2, upgradeToItemId: 3}
+		]
+	})
+	
+	it('should return all itemIds if purchasedItemIds is empty', () => {
+		expect([...Filter.purchaseableItems(new Set(),itemIds,exclusions,upgrades)].sort())
+			.toEqual([1,2,3,4])
+	})
+	it('should return all itemIds except the excluded ones when there are no upgrades', () => {
+		expect([...Filter.purchaseableItems(new Set([1]),itemIds,exclusions,upgrades)].sort())
+			.toEqual([3,4])
+	})
+	it('should function properly in a vareity of non-edge cases', () => {
+		expect([...Filter.purchaseableItems(new Set([2]),itemIds,exclusions,upgrades)].sort())
+			.toEqual([4])
+		expect([...Filter.purchaseableItems(new Set([1,2]),itemIds,exclusions,upgrades)].sort())
+			.toEqual([4])
+	})
+})
+
+describe('enabledPurchaseableItems', () => {
+	beforeEach(() => {
+		itemIds = new Set([1,2,3,4])
+		exclusions = [
+			{excluderItemId: 1, excludedItemId: 1},
+			{excluderItemId: 2, excludedItemId: 2},
+			{excluderItemId: 3, excludedItemId: 3},
+			{excluderItemId: 4, excludedItemId: 4},
+			{excluderItemId: 1, excludedItemId: 2},
+		]
+		upgrades = [
+			{upgradeFromItemId: 2, upgradeToItemId: 1},
+		]
+		qualifications = [
+			{qualifierItemId: 1, qualifiedItemId: 4},
+			{qualifierItemId: 2, qualifiedItemId: 4},
+		]
+	})
+
+	it(`should return purchaseableItems that are not excludedItems
+		 or upgradeToItems of the currently selected items, for which the
+		 user is eligible based on purchased items and selected items`, () => {
+
+		let wrapEnabledPurchaseableItems = (purchasedItemIds,selectedItemIds) => {
+			return [...Filter.enabledPurchaseableItems(
+			 	purchasedItemIds,
+			 	selectedItemIds,
+			 	itemIds,
+			 	exclusions,
+			 	upgrades,
+			 	qualifications)]
+			.sort()
+		}
+		expect(wrapEnabledPurchaseableItems(new Set(), new Set())).toEqual([1,2,3])
+		expect(wrapEnabledPurchaseableItems(new Set([1]), new Set())).toEqual([3,4])
+		expect(wrapEnabledPurchaseableItems(new Set(), new Set([1]))).toEqual([1,3,4])
+		expect(wrapEnabledPurchaseableItems(new Set([1]), new Set([3]))).toEqual([3,4])
+		expect(wrapEnabledPurchaseableItems(new Set([1,3]), new Set())).toEqual([4])
+		expect(wrapEnabledPurchaseableItems(new Set([1,3,4]), new Set())).toEqual([])
+		expect(wrapEnabledPurchaseableItems(new Set(), new Set([1,3,4]))).toEqual([1,3,4])
+		expect(wrapEnabledPurchaseableItems(new Set(), new Set([1,4]))).toEqual([1,3,4])
+	})
+})
