69c8aa879491b8e7496ed5ad3cd03343d56f659d
Chad Mills <chad.mills@gmail.com>
2018-09-27 04:58:04 -0400

    remove unused purchased_items function

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,20 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :full_name,
-  :email#, :purchased_items
-
-  # has_many :payments
-
-  def purchased_items
-    sales = object.sales
-      .select{|sale| !sale.void}
-      .map{|sale| {
-      id: sale.item.id,
-      item: sale.item.name,
-      purchase_price: sale.price.amount.to_f,
-      price_type: sale.price.price_type,
-      sale_id: sale.id
-    }}
-
-  end
-
+  attributes :id, :first_name, :last_name, :full_name, :email
 end
