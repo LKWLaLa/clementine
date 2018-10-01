c12c1209e577bd4acf12f4546ef5fac989be7870
Chad Mills <chad.mills@gmail.com>
2018-10-01 01:03:44 -0400

    initialize Kinship db with all declared models prior to creating instances

--- app/javascript/kinship/Kinship.js
+++ app/javascript/kinship/Kinship.js
@@ -5,4 +5,11 @@ Kinship.Record = _.Record
 Kinship.resetDb = _.resetDb
 Kinship.getDb = _.getDb
 
+Kinship.initializeDb = (models) => {
+	db = Kinship.getDb()
+	for (let model of models) {
+		db[model.name] = {records: {}, instances: {}}
+	}
+}
+
 module.exports = Kinship
