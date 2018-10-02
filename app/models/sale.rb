85b300172304d84669f8db6e021ec84f5001d212
Chad Mills <chad.mills@gmail.com>
2018-10-02 18:56:46 -0400

    validations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,6 +4,8 @@ class Sale < ApplicationRecord
   belongs_to :item
   belongs_to :payment
 
+  validates_presence_of :price_id, :user_id, :item_id
+
   #display name for active admin
   def display_name
     id
