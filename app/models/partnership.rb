aa0803a86a151c539228c600308b16127452836a
LKWLaLa <lkwlala@gmail.com>
2018-10-09 02:30:02 -0400

    add partnerships to active admin

--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -1,6 +1,8 @@
 class Partnership < ApplicationRecord
   belongs_to :sale
   delegate :buyer, to: :sale
+  has_one :buyer, through: :sale
   delegate :item, to: :sale
+  has_one :item, through: :sale
   belongs_to :invitee, class_name: :User, foreign_key: "invitee_id"
 end
