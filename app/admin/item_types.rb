d9275dea9f38ad323dfd95fc15bc7bf88c7407f6
Chad Mills <chad@Chads-MacBook-Air.local>
2019-03-29 19:05:07 -0400

    spacing

--- app/admin/item_types.rb
+++ app/admin/item_types.rb
@@ -1,35 +1,35 @@
 ActiveAdmin.register ItemType do
 
-actions :index, :show, :new, :create, :edit, :update
-permit_params :name
+  actions :index, :show, :new, :create, :edit, :update
+  permit_params :name
 
-index do
-  column :name
-  column :description
-  column :current_price do |it|
-    number_to_currency(it.current_price.amount)
-  end
-  column :sold_out
-  actions
-end 
-
-show do
-  attributes_table do
-    row :name
-    row :description
-    row :current_price do |it|
+  index do
+    column :name
+    column :description
+    column :current_price do |it|
       number_to_currency(it.current_price.amount)
     end
-    row :sold_out
-    row :created_at
-    end
-     panel "Items included" do
-      table_for item_type.items do
-        column "item" do |item|
-          link_to item.name, admin_item_path(item)
-        end
-        column "description" do |item|
-          item.description
+    column :sold_out
+    actions
+  end 
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :current_price do |it|
+        number_to_currency(it.current_price.amount)
+      end
+      row :sold_out
+      row :created_at
+      end
+       panel "Items included" do
+        table_for item_type.items do
+          column "item" do |item|
+            link_to item.name, admin_item_path(item)
+          end
+          column "description" do |item|
+            item.description
         end
       end
     end
